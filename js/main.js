02e8180353f9f048199e0e41a243d0ad177e2547
SaraMercado10 <sara.denise.mercado.12@gmail.com>
2026-05-02 13:22:49 -0300

    Index.html

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// modo oscuro
+const toggle = document.getElementById("darkModeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// MENÚ HAMBURGUESA
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.querySelector(".nav ul");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+// CONTADOR
+const contador = document.getElementById("contador");
+let iniciado = false;
+
+window.addEventListener("scroll", () => {
+  if (!contador) return;
+
+  const posicion = contador.getBoundingClientRect().top;
+  const pantalla = window.innerHeight;
+
+  if (posicion < pantalla && !iniciado) {
+    iniciado = true;
+
+    let valor = 0;
+    const max = 500;
+
+    const intervalo = setInterval(() => {
+      valor += 5;
+      contador.textContent = valor;
+
+      if (valor >= max) {
+        contador.textContent = max;
+        clearInterval(intervalo);
+      }
+    }, 20);
+  }
+});
